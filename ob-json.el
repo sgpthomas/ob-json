1dd15f1f6a5fe3343998f538950bdb1573d96949
Samuel Thomas <sgpthomas@gmail.com>
2022-04-29 10:48:17 -0500

    add json mode

--- ob-json.el
+++ ob-json.el
@@ -2,3 +2,7 @@
 
 (defun org-babel-execute:json (body params)
   (org-babel-eval "jq -M" body))
+
+;;;###autoload
+(define-derived-mode json-mode javascript-mode "JSON Mode"
+  "Major Mode for highlighting json org babel blocks.")
